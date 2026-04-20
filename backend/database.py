b2db32781bdcf3bbf38bf0994fb89b6fd25d79d0
anni88git <anirudh.chopra.cs27@iilm.edu>
2026-04-20 09:33:30 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
 import os
+from pymongo import MongoClient
+import certifi
 from dotenv import load_dotenv
 
-# This loads local variables if you are on your laptop, but ignores it on Render
-load_dotenv() 
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
 
-# This is the MAGIC line. It pulls the string securely from Render's environment
-MONGO_URI = os.getenv("MONGO_URI") 
-
-# Then you pass it to your client
-client = MongoClient(MONGO_URI)
+# The tlsCAFile handles the SSL handshake that Render requires
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+db = client["NetworxDB"]
